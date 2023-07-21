223dc72817ec014cdfd126afc0520a303b18dfad
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2023-07-21 12:51:51 -0700

    Only provide an es build for language

--- codemirror-lang-sequence/rollup.config.js
+++ codemirror-lang-sequence/rollup.config.js
@@ -4,9 +4,6 @@ import { lezer } from '@lezer/generator/rollup';
 export default {
   external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
   input: 'src/index.ts',
-  output: [
-    { file: 'dist/index.cjs', format: 'cjs' },
-    { dir: './dist', format: 'es' },
-  ],
+  output: [{ dir: './dist', format: 'es' }],
   plugins: [lezer(), typescript()],
 };
