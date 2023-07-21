77cb567c32495d042a038ead503ca8bcd2626136
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2023-07-21 11:55:05 -0700

    Add minification options to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  build: { minify: true },
   css: { devSourcemap: true },
   plugins: [sveltekit()],
   resolve: { alias: { stream: 'stream-browserify' } },
