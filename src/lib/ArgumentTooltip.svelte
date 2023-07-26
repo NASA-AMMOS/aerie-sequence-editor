4a682039138f6b5cc90659df298304fe5eb683d9
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2023-07-26 10:28:26 -0700

    Add more tooltip info for command arguments

--- src/lib/ArgumentTooltip.svelte
+++ src/lib/ArgumentTooltip.svelte
@@ -8,11 +8,41 @@
 
 <div class="argument-tooltip">
   <div class="container">
-    Argument Name: {arg.name}
-    <br />
-    Description: {arg.description}
+    Name: {arg.name}
     <br />
     Type: {arg.arg_type}
+    <br />
+    Description: {arg.description}
+
+    {#if arg.arg_type === 'enum'}
+      <br />
+      Enum Name: {arg.enum_name}
+    {/if}
+
+    {#if arg.arg_type === 'boolean' || arg.arg_type === 'enum' || arg.arg_type === 'float' || arg.arg_type === 'integer' || arg.arg_type === 'numeric' || arg.arg_type === 'time' || arg.arg_type === 'unsigned' || arg.arg_type === 'var_string'}
+      <br />
+      Default Value: {arg.default_value ?? 'None'}
+    {/if}
+
+    {#if arg.arg_type === 'float' || arg.arg_type === 'integer' || arg.arg_type === 'numeric' || arg.arg_type === 'unsigned'}
+      <br />
+      Range: {arg.range ? `[${arg.range.min}, ${arg.range.max}]` : 'None'}
+    {/if}
+
+    {#if arg.arg_type === 'float' || arg.arg_type === 'integer' || arg.arg_type === 'numeric' || arg.arg_type === 'time' || arg.arg_type === 'unsigned'}
+      <br />
+      Units: {arg.units === 'none' ? 'None' : arg.units}
+    {/if}
+
+    {#if arg.arg_type === 'boolean' || arg.arg_type === 'enum' || arg.arg_type === 'float' || arg.arg_type === 'integer' || arg.arg_type === 'numeric' || arg.arg_type === 'time' || arg.arg_type === 'unsigned'}
+      <br />
+      Bit Length: {arg.bit_length ?? 'None'}
+    {/if}
+
+    {#if arg.arg_type === 'repeat' || arg.arg_type === 'var_string'}
+      <br />
+      Prefix Bit Length: {arg.prefix_bit_length ?? 'None'}
+    {/if}
     <br />
   </div>
 </div>
