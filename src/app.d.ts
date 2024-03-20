117009180ce48ab7413ac4a9029d2fecf9e810ea
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2024-03-20 10:23:41 -1000

    First pass at adding a custom adaptation js file, includes global support and custom linting

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,10 @@
+import type { GlobalType } from './types/global-type';
+
 declare global {
   namespace App {}
+  // eslint-disable-next-line no-var
+  var GLOBALS: GlobalType[];
+  function LINT(commandDictionary, view, node);
 }
 
 export {};
