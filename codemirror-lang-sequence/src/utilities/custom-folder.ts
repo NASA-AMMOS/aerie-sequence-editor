9231b206738c54855a7cccb8daea3d597b4e191d
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2024-03-27 10:32:43 -1000

    Color Highting and Custom Folding
    * Color highlight specific grammar keywords
    * Add a custom folder for commands

--- codemirror-lang-sequence/src/utilities/custom-folder.ts
+++ codemirror-lang-sequence/src/utilities/custom-folder.ts
@@ -10,38 +10,30 @@ export function customFoldInside(node: SyntaxNode): { from: number; to: number }
 function foldCommand(node: SyntaxNode): { from: number; to: number } | null {
   const stemNode = node.getChild('Stem');
   const argsNodes = node.getChildren('Args');
+  const commetNode = node.getChild('LineComment');
   const metadataNode = node.getChildren('Metadata');
-  const modelNodes = node.getChildren('Model');
+  const modelNodes = node.getChildren('Models');
 
   if (stemNode == null) {
     return null;
   }
 
-  let from = calculateStartAndEnd([stemNode]).to;
-  if (argsNodes.length > 0) {
-    from = calculateStartAndEnd(argsNodes).to;
-  }
-
-  // determine which node starts sooner if both exist
-  if (metadataNode.length > 0 && modelNodes.length > 0) {
-    const { to: to } = calculateStartAndEnd(modelNodes.concat(metadataNode));
-    return { from, to };
-  } else if (metadataNode) {
-    const { to: to } = calculateStartAndEnd(metadataNode);
-    return { from, to };
-  } else if (modelNodes) {
-    const { to: to } = calculateStartAndEnd(modelNodes);
-    return { from, to };
-  }
-  return null;
+  const from = calculateStartAndEnd([stemNode, ...argsNodes, commetNode]).to;
+  const to = calculateStartAndEnd([...metadataNode, ...modelNodes]).to;
+  return { from, to };
 }
 
-function calculateStartAndEnd(nodes: SyntaxNode[]): { from: number; to: number } {
+function calculateStartAndEnd(nodes: (SyntaxNode | null)[]): { from: number; to: number } {
   return nodes.reduce(
-    (acc, node) => ({
-      from: Math.min(acc.from, node.from),
-      to: Math.max(acc.to, node.to),
-    }),
+    (acc, node) => {
+      if (node === null) {
+        return acc;
+      }
+      return {
+        from: Math.min(acc.from, node.from),
+        to: Math.max(acc.to, node.to),
+      };
+    },
     { from: Number.MAX_VALUE, to: Number.MIN_VALUE },
   );
 }
